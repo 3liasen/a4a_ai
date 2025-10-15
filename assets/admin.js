c7fabb9df9284782160f96c4d94adf2a4fd454df
3liasen <jan.eliasen@gmail.com>
2025-10-15 16:22:43 +0200

    Update Font Awesome integrity hash in admin.js

--- assets/admin.js
+++ assets/admin.js
@@ -35,7 +35,7 @@
   injectStylesheet('https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css');
   injectStylesheet(
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css',
-    'sha512-jQFNUPYjZ6eKeosFVJeFt3uCiL6A+O9rDNr/g0x3hCwewJYHZVu8dxPMH6b9Y8u61QY4VvZ8a2S7k+Jf1C9v0g=='
+    'sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=='
   );
 
   const baseStyle = document.createElement('style');
