fcff1a09900892f4cace3fcb7f8dcdda36e1eb57
3liasen <jan.eliasen@gmail.com>
2025-10-22 10:07:09 +0200

    feat: Initialize new REST router in plugin constructor

--- a4a_ai.php
+++ a4a_ai.php
@@ -44,6 +44,10 @@ final class A4A_AI_Plugin {
 
     private function __construct() {
         add_action('init', [$this, 'register_post_types']);
+        // Initialize new REST router
+        if ( class_exists('A4A_AI_REST') ) {
+            ( new A4A_AI_REST() )->init();
+        }
         add_action('rest_api_init', [$this, 'register_rest_routes']);
         add_action('admin_menu', [$this, 'register_admin_menu'], 20);
         add_action('admin_enqueue_scripts', [$this, 'enqueue_admin_assets']);
@@ -1896,6 +1900,8 @@ foreach ( [
         require_once $p;
     }
 }
+
+
 
 
 
